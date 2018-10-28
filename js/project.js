9ea3986f605b2c24fff59a617faccb820391fc84
glumac <mglumac@gmail.com>
2018-10-28 08:06:21 -0400

    remove jquery

--- js/project.js
+++ js/project.js
@@ -1,25 +1,22 @@
-// 1. use jQuery to assign the .click event to #color-button
-$('#color-button').click(changeColor);
-// 2. Write a function, named changeColor, that is called when #color-button is clicked
+// 1. Select #color-button from the page, and add an onclick event handler that triggers a functin changeColor;
+document.querySelector('#color-button').onclick = changeColor;
 
+// 2. Write the function, named changeColor, that is called when #color-button is clicked
 function changeColor() {
-  var red = $('#red').val();
-  var green = $('#green').val();
-  var blue = $('#blue').val();
-  var colorString = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  // 3. Inside of the function changeColor, use variables to store the <input> values from #red, #green, and #blue
+  // - Use .value to get the values from the inputs
+  // HINT: var red = document.getElementById('red').value;
+  const red = document.querySelector('#red').value;
+  const green = document.querySelector('#green').value;
+  const blue = document.querySelector('#blue').value;
 
-  $('#colorful-text').html(colorString);
-  $('#wrapper').css('background', colorString);
+  // 4. Create a variable, named colorStr, which concatenates the above red, green, and blue variables into the format:
+  // rgb(x, y, z)
+  // - If the user enters: 100, 150, and 200, colorStr should be: rgb(100, 150, 200)
+  const colorString = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+  // 5. Use .innerHtml to change the text inside of #colorful-text to colorStr
+  document.querySelector('#colorful-text').innerHtml = colorString;
+  // 6. Use .style.background to change the background of #wrapper to colorStr
+  document.querySelector('#wrapper').style.background = colorString;
 }
-
-// 3. Inside of the function changeColor, use variables to store the <input> values from #red, #green, and #blue
-// - Use .value to get the values from the inputs
-// HINT: var red = document.getElementById('red').value;
-
-// 4. Create a variable, named colorStr, which concatenates the above red, green, and blue variables into the format:
-// rgb(x, y, z)
-// - If the user enters: 100, 150, and 200, colorStr should be: rgb(100, 150, 200)
-
-// 5. Use .html() to change the text inside of #colorful-text to colorStr
-
-// 6. Use .css(background: xyz) to change the background of #wrapper to colorStr
