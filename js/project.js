917091ef8029d19847c72f70da5f9a0a35223d0f
Michael Glumac <mglumac@gmail.com>
2019-05-19 13:22:00 -0400

    queryselector

--- js/project.js
+++ js/project.js
@@ -1,15 +1,15 @@
-// 1. Select #color-button from the page, and add an onclick event handler that triggers a functin changeColor;
+// 1. Select #color-button from the page, and add an onclick event handler that triggers a function changeColor;
 
 // 2. Write the function, named changeColor, that is called when #color-button is clicked
 
 // 3. Inside of the function changeColor, use variables to store the <input> values from #red, #green, and #blue
 // - Use .value to get the values from the inputs
-// HINT: var red = document.getElementById('red').value;
+// HINT: var red = document.querySelector('#red').value;
 
 // 4. Create a variable, named colorStr, which concatenates the above red, green, and blue variables into the format:
 // rgb(x, y, z)
 // - If the user enters: 100, 150, and 200, colorStr should be: rgb(100, 150, 200)
 
-// 5. Use .innerHtml to change the text inside of #colorful-text to colorStr
+// 5. Use .innerHTML to change the text inside of #colorful-text to colorStr
 
 // 6. Use .style.background to change the background of #wrapper to colorStr
